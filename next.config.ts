c7ac6ba127a0ac68610b7770be7954404a1ded86
heaeat <heaeat@naver.com>
2026-04-29 16:22:23 +0900

    remove next.config redirects (rely on client-side only)

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/map",
-        permanent: false,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
